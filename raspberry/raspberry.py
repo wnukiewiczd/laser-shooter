4abe8d13cd0cbd3fc290b43e1cae4fe4a969fdcd
Dawid Wnukiewicz <dawid.wnukiewicz@evolpe.pl>
2024-12-22 20:47:39 +0100

    dodanie raspberry kodu

---
+++ raspberry/raspberry.py
@@ -0,0 +1,66 @@
+import random
+import paho.mqtt.client as mqtt
+
+# Zmienna globalna przechowująca wylosowany Wemos
+selected_wemos = None
+
+#Zmienna globalna przechowująca wynik gracza
+playerScore = 0
+
+# Funkcja obsługi wiadomości
+def on_message(client, userdata, message):
+    global playerScore
+    # Wemos1 został trafiony
+    if message.topic == "wemos1/lightRead" and message.payload.decode() == "hit" and selected_wemos == "Wemos1":
+        print("Cel 1 trafiony")
+        playerScore = playerScore + 1
+        start(client)
+    # Wemos2 został trafiony
+    elif message.topic == "wemos2/lightRead" and message.payload.decode() == "hit" and selected_wemos == "Wemos2":
+        print("Cel 2 trafiony")
+        playerScore = playerScore + 1
+        start(client)
+    # Wemos3 został trafiony
+    elif message.topic == "wemos3/lightRead" and message.payload.decode() == "hit" and selected_wemos == "Wemos3":
+        print("Cel 3 trafiony")
+        playerScore = playerScore + 1
+        start(client)
+
+# Funkcja start losująca Wemosa i wysyłająca wiadomość
+def start(client):
+    global selected_wemos
+    # Losowanie Wemosa
+    selected_wemos = random.choice(["Wemos1", "Wemos2", "Wemos3"])
+    # Ustawienie odpowiedniego tematu
+    topic = f"raspberry/{selected_wemos}/ledStrip" 
+    # Wysłanie wiadomości "turnOnGreen"
+    client.publish(topic, "turnOnGreen")
+    print(f"Wysłano wiadomość 'turnOnGreen' do {selected_wemos} na temat {topic}")
+
+    # Funkcja start losująca Wemosa i wysyłająca wiadomość
+def stop(client):
+    client.publish("raspberry/WemosAll/ledStrip", "stop")
+    print(f"Wysłano wiadomość 'stop' do wszystkich wemosów")
+    selected_wemos = None
+    playerScore = 0
+
+# Konfiguracja klienta MQTT
+client = mqtt.Client()
+client.on_message = on_message
+client.connect("192.168.4.2", 1883, 60)
+
+# Subskrypcje tematów dla Wemosów
+client.subscribe("wemos1/lightRead")
+client.subscribe("wemos2/lightRead")
+client.subscribe("wemos3/lightRead")
+
+# Start pętli odbierania wiadomości
+client.loop_start()
+
+try:
+    # Wywołanie funkcji start
+    start(client)
+    while True:
+        pass  # Program działa ciągle
+except KeyboardInterrupt:
+    client.loop_stop()
---
+++ raspberry/wemos_1.ino
@@ -0,0 +1,164 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+//Komunikacja bezprzewodowa
+const char* ssid = "projekt_AP";  // Nazwa sieci WiFi Raspberry Pi
+const char* password = "malinka3"; // Hasło do sieci WiFi
+const char* mqtt_server = "192.168.4.2";  // Adres IP Raspberry Pi (broker MQTT)
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Deklaracja pinów dla paska LED
+const int pinRed = D2;    // Pin sterujący kolorem czerwonym
+const int pinGreen = D1;  // Pin sterujący kolorem zielonym
+const int pinBlue = D5;   // Pin sterujący kolorem niebieskim
+
+//Kolory pasek LED
+#define turnOff 0
+#define red 1
+#define green 2
+
+//Deklaracja pinu dla fototranzystora
+int photoTransistor = A0;
+
+
+
+void setup() {
+ 
+  Serial.begin(115200);
+  
+
+  //Nawiązywanie połączenia po wifi
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(onMessage);
+  reconnect();
+
+  //Konfiguracja pinow dla paska LED
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinBlue, OUTPUT);
+  // Pasek LED w stanie początkowym ma być zgaszony
+  analogWrite(pinRed, 0);
+  analogWrite(pinGreen, 0);
+  analogWrite(pinBlue, 0);
+
+
+}
+
+
+
+void loop() {
+  if (!client.connected()) {
+   reconnect();
+  }
+  
+  client.loop();
+  
+  int lightRead = analogRead(photoTransistor);
+  Serial.print(lightRead);                
+      if (lightRead < 300) {
+        Serial.print("Cel 1 trafiony");  
+        client.publish("wemos1/lightRead", "hit"); // Wysłanie wiadomości do raspberry o trafieniu w cel
+        setColor(turnOff); // Zgaszenie paska LED
+      }
+    
+      Serial.println("");
+      delay(100);      // !!! Co ile czyta wartości z fototranzytora
+
+
+}
+
+//Funkcja wyzwalana po otrzymaniu wiadomości od raspberry
+void onMessage(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+
+
+
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  Serial.println(message);
+
+  
+  if (message == "turnOnGreen") {
+   setColor(green);                  //Zapal pasek led na zielono
+  } else if (message == "turnOnRed") {
+   setColor(red);                    //Zapal pasek led na czerwono
+  } else if (message == "stop") {
+   setColor(turnOff);
+  }
+
+}
+
+
+
+//Obsługa komunikacji przez wifi
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("WemosClient1")) {
+      client.subscribe("raspberry/Wemos1/ledStrip");  // Subscribe to the topic for receiving data
+      client.subscribe("raspberry/WemosAll/ledStrip");
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+//Sterowanie paskiem LED
+void setColor(int colorName) {
+ 
+ if(colorName == green) {
+  analogWrite(pinRed, 255);
+  analogWrite(pinGreen, 0);
+  analogWrite(pinBlue, 0);
+ } 
+
+ if (colorName == red) {
+  analogWrite(pinRed, 0);
+  analogWrite(pinGreen, 255);
+  analogWrite(pinBlue, 0);
+ }
+ 
+ if (colorName == turnOff) {
+  analogWrite(pinRed, 0);
+  analogWrite(pinGreen, 0);
+  analogWrite(pinBlue, 0);
+ }
+
+}
+
+void rainbow() {
+  unsigned long startTime = millis(); // Pobierz aktualny czas
+  while (millis() - startTime < 2000) { // Pętla przez 2 sekundy
+    // Generowanie losowych wartości dla kolorów
+    int redValue = random(0, 255);
+    int greenValue = random(0, 255);
+    int blueValue = random(0, 255);
+
+    // Ustawianie wyjściowych wartości RGB
+    analogWrite(pinRed, redValue);
+    analogWrite(pinGreen, greenValue);
+    analogWrite(pinBlue, blueValue);
+
+    delay(100); // Krótkie opóźnienie między zmianami kolorów
+  }
+}
+
+
+
